17d54bf9e82b05465ff31c290ffd912493562ce2
Louis Li <zswenjin.li@me.com>
2023-02-21 21:18:31 +0800

    update .gitignore

--- dist/mz.datav.es.js
+++
@@ -1,7 +0,0 @@
-import { random, a, b } from 'sam-test-data';
-export { random as default } from 'sam-test-data';
-
-//common js写法
-
-
-console.log(random(100), a, b );
--- src/index.js
+++ src/index.js
@@ -1,15 +1,3 @@
-//common js写法
-// const data = require('sam-test-data')
 
-
-// console.log(data.random(), data.a, data.b)
-
-// module.exports = {}
-
-//es js写法
-import { random, a, b } from 'sam-test-data'
-
-
-console.log(random(100), a, b )
 
 export default random
