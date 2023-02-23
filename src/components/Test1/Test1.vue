d8e003203b004f69a1e8fa54a0868de183da5b2f
Louis Li <zswenjin.li@me.com>
2023-02-23 20:54:18 +0800

    svg组件开发

--- src/components/Test1/Test1.vue
+++ src/components/Test1/Test1.vue
@@ -1,5 +1,24 @@
 <template>
-  <div>
+  <svg width="0" height="0" viewBox="0 0 100 100">
+    <defs>
+      <symbol id="more" viewBox="0 0 100 100">
+        <circle r="5" cx="20" cy="25" fill="currentColor" />
+        <circle r="5" cx="20" cy="50" fill="currentColor" />
+        <circle r="5" cx="20" cy="75" fill="currentColor" />
+        <line x1="40" y1="25" x2="90" y2="25" stroke-width="8" stroke="currentColor" />
+        <line x1="40" y1="50" x2="90" y2="50" stroke-width="8" stroke="currentColor" />
+        <line x1="40" y1="75" x2="90" y2="75" stroke-width="8" stroke="currentColor" />
+      </symbol>
+      <symbol id="filledArrowRight" viewBox="0 0 100 100">
+        <polyline points="20 10, 80 50, 20 90" fill="currentColor"></polyline>
+      </symbol>
+      <symbol id="arrowRight" viewBox="0 0 100 100">
+        <polyline points="30 20, 70 50, 30 80" fill="transparent" stroke="currentColor" stroke-width="3"></polyline>
+      </symbol>
+    </defs>
+  </svg>
+
+  <!-- <div>
     <svg
       width="500"
       height="200"
@@ -40,7 +59,7 @@
         style="stroke: #000000; fill: none"
       />
     </svg>
-  </div>
+  </div> -->
 </template>
 
 <script>
